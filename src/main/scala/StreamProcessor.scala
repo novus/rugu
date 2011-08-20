14718f9190ef1fecedc1740f04f502d3fa54fb4a
Chris Lewis <chris@thegodcode.net>
2011-08-20 12:42:26 -0400

    broken sketch

--- src/main/scala/StreamProcessor.scala
+++ src/main/scala/StreamProcessor.scala
@@ -14,7 +14,7 @@ trait StreamProcessor[O] extends (InputStream => O) {
 }
 
 object SP {
-  def sp[I](p: InputStream => I) = new StreamProcessor[Either[I, I]] {
+  def managed[I](p: InputStream => I) = new StreamProcessor[Either[I, I]] {
     def apply(in: InputStream) = Right(p(in))
   }
   
@@ -22,15 +22,15 @@ object SP {
     def apply(in: InputStream) = Left(p(in))
   }
   
-  //implicit val AsBufferedReader = sp(in => new BufferedReader(new InputStreamReader(in)))
+  //implicit val AsBufferedReader = managed(in => new BufferedReader(new InputStreamReader(in)))
   
-  /*implicit val AsSource         = sp(in => Source.fromInputStream(in))
-  implicit val AsListString     = sp(in => AsSource(in).getLines.toList)
-  implicit val AsUnit           = sp(_ => ())
+  /*implicit val AsSource         = managed(in => Source.fromInputStream(in))
+  implicit val AsListString     = managed(in => AsSource(in).getLines.toList)
+  implicit val AsUnit           = managed(_ => ())
   // TODO this is stupid.
-  implicit val AsInt            = sp(in => AsListString(in).headOption.map(_ toInt))
-  implicit val AsString         = sp(in => AsSource(in).mkString)*/
+  implicit val AsInt            = managed(in => AsListString(in).headOption.map(_ toInt))
+  implicit val AsString         = managed(in => AsSource(in).mkString)*/
   
-  implicit val AsListString     = sp(in => Source.fromInputStream(in).getLines.toList)
+  implicit val AsListString     = managed(in => Source.fromInputStream(in).getLines.toList)
   implicit val AsInputStream    = unmanaged(in => () => in)
 }
