d2ecc0151e1189f87a44f812db3d0764be17e089
Chris Lewis <chris@thegodcode.net>
2011-08-30 11:16:51 -0400

    modularized underlying ssh lib

--- src/main/scala/Ssh.scala
+++ src/main/scala/Ssh.scala
@@ -14,7 +14,7 @@ object Ssh {
   }
   
   def apply(host: Host, auth: Authentication, knownHostsFile: Option[String] = None) = {
-    val factoryBase =
+    val executorBase =
       (_:Unit) => {
         val jsch = new SSHClient()
         knownHostsFile.foreach(f => jsch.loadKnownHosts(new java.io.File(f))) //FIXME don't load every time!
@@ -22,20 +22,20 @@ object Ssh {
         jsch
       }
     
-    val factory = auth match {
+    val executor = auth match {
       case UsernameAndPassword(u, p) =>
         new Executor {
           def apply[A](command: Command[_, A])(f: java.io.InputStream => A): Either[Throwable, (Option[Int], A, String)] = {
             allCatch.either {
-              val ssh = factoryBase(())
+              val ssh = executorBase(())
               ssh.authPassword(u, p)
               val s = ssh.startSession()
-              val c = s.exec(command.command)
-              command.input.foreach { in => c.getOutputStream.write(in.getBytes()) }
               
+              command.input.foreach { in => s.getOutputStream.write(in.getBytes()) }
+              val c = s.exec(command.command)
+              val a = f(c.getInputStream())
               val err = scala.io.Source.fromInputStream(c.getErrorStream()).mkString
               c.join(5, java.util.concurrent.TimeUnit.SECONDS) //TODO
-              val a = f(c.getInputStream())
               s.close()
               ssh.disconnect()
               (Option(c.getExitStatus).map(_.intValue), a, err)
@@ -43,11 +43,11 @@ object Ssh {
           }
         }
     }
-    new SshSession(factory)
+    new SshSession(executor)
   }
 }
 
-class SshSession(factory: Ssh.Executor) {
+class SshSession(executor: Ssh.Executor) {
   def apply[I, O](c: Command[I, O])(implicit sp: StreamProcessor[I]) =
     exec(c)(identity).fold(
       Left(_),
@@ -56,13 +56,9 @@ class SshSession(factory: Ssh.Executor) {
   def exec[I, O, OO](c: Command[I, O])(f: ((Option[Int], O, String)) => OO)(implicit sp: StreamProcessor[I]) =
     remote(c, sp).fold(Left(_), r => Right(f(r)))
   
-  /* Get a new, authorized session and prepare a channel for invoking
-   * shell commands. Yield the active session, (executed) channel, and a
-   * closure to clean them up.
-   */
   /* Left[Throwable] on network error. */
   def remote[I, O](c: Command[I, O], sp: StreamProcessor[I]): Either[Throwable, (Option[Int], O, String)] =
-    factory(c) { sp andThen c }
+    executor(c)(sp andThen c)
 }
 
 object IO {
